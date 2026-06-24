9d93e3a856057187b94057e85c38cbc5ecabf896
msalomon <msalomon@ciat.edu>
2026-06-23 21:17:02 -0700

    Added student discount logic

--- MovieTicket.py
+++ MovieTicket.py
@@ -1,6 +1,7 @@
 # Movie Ticket Price Calculator
 
 age = int(input("Enter your age: "))
+student = input("Are you a student? (Y/N): ")
 
 if age < 13:
     price = 8
@@ -8,5 +9,8 @@ elif age >= 65:
     price = 10
 else:
     price = 12
+
+if student == "Y":
+    price = price - 2
 
 print("Ticket price: $", price)
